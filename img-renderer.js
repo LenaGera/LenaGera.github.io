9b27674bca5d5db9002950378870fbe65ff09ffe
LenaGera <141572351+LenaGera@users.noreply.github.com>
2023-08-20 14:19:16 +0300

    Add files via upload

---
+++ img-renderer.js
@@ -0,0 +1,32 @@
+function getImages() {
+    const files = [];
+    for (let i = 0; i < 12; i++) {
+        files.push(`images/img${i}.jpg`);
+    }
+    return files;
+}
+
+const imageFiles = getImages();
+
+function renderImages() {
+    const imageContainer1 = document.getElementById('imgContainer1');
+    const imageContainer2 = document.getElementById('imgContainer2');
+
+    for (let i = 0; i < Math.ceil(imageFiles.length / 2.0); i++) {
+        const imgElement = document.createElement('img');
+        imgElement.src = imageFiles[i];
+        imgElement.alt = 'Image'; // Set an alt attribute for accessibility
+        imgElement.className = 'img-container m-2';
+        imageContainer1.appendChild(imgElement);
+    }
+    for (let i = Math.ceil(imageFiles.length / 2.0); i < imageFiles.length; i++) {
+        const imgElement = document.createElement('img');
+        imgElement.src = imageFiles[i];
+        imgElement.alt = 'Image'; // Set an alt attribute for accessibility
+        imgElement.className = 'img-container m-2';
+        imageContainer2.appendChild(imgElement);
+    }
+}
+
+// Call the renderImages function when the page loads
+document.addEventListener('DOMContentLoaded', renderImages);
